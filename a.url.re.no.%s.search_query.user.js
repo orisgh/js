ecc6ca7c82b086527b3d478efbfcccb12763b607
orisgh <44023550+orisgh@users.noreply.github.com>
2023-11-07 20:46:57 +0800

    2023.1107

---
+++ a.url.re.no.%s.search_query.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+
+// @name               a.url.re.no.%s.search_query
+// @downloadURL        https://github.com/orisgh/nox.user.js/raw/main/a.url.re.no.%s.search_query.user.js
+// @updateURL          https://github.com/orisgh/nox.user.js/raw/main/a.url.re.no.%s.search_query.user.js
+// @run-at             document-start
+// @version            2023.1107
+
+// @description        w
+// @author             nox
+// @license            MIT
+// @namespace          https://github.com/orisgh
+// @copyright          2023, nox (https://github.com/orisgh)
+// @homepageURL        https://github.com/orisgh/nox.user.js
+
+// @noframes
+// @grant              none
+// @connect            none
+// @require            none
+
+// @match              https://search.bilibili.com/*
+// @match              https://t.bilibili.com/*
+
+// @match              https://www.youtube.com/results?search_query=*
+// @match              https://www.google.com/search?q=*
+// @match              https://www.wuzhuiso.com/s?q=*
+// @match              w
+// @match              w
+// @match              w
+// @match              w
+// @match              w
+// @match              w
+// @match              w
+// @match              w
+// @match              w
+
+// ==/UserScript==
+
+function aaaaaaaa() {
+
+    const whitelist = [
+        "keyword",
+        "tab",
+        "search_query",
+        "q",
+        "v",
+        "query",
+        "text",
+    ];
+
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+
+    for (let param of params.keys()) {
+        if (!whitelist.includes(param)) {
+            params.delete(param);
+        }
+    }
+
+    url.search = params.toString();
+
+    url.pathname = url.pathname.replace(/\/$/, '');
+
+    window.history.replaceState(null, null, url.toString());
+
+}
+
+aaaaaaaa();
+
+document.addEventListener("visibilitychange", function () {
+    if (document.visibilityState === "visible") {
+        aaaaaaaa();
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Control') {
+        if (!event.repeat) {
+            aaaaaaaa();
+        }
+    }
+});
