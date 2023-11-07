3b1d4f5af3d16aa26772fba6b0b9baa0a7f36084
orisgh <44023550+orisgh@users.noreply.github.com>
2023-11-07 16:48:54 +0800

    Create a.bili.online.ads.up.user.js
    2023.1107

---
+++ a.bili.online.ads.up.user.js
@@ -0,0 +1,136 @@
+// ==UserScript==
+
+// @name               a.bili.online.ads.up
+// @run-at             document-idle
+// @version            2023.1107
+
+// @description        w
+// @license            MIT
+// @author             nox
+// @copyright          2023, nox (https://github.com/orisgh)
+// @namespace          https://github.com/orisgh
+// @homepageURL        https://github.com/orisgh/nox.user.script
+// @downloadURL        https://raw.githubusercontent.com/orisgh/nox.user.js/master/dist/w.user.js
+// @updateURL          https://raw.githubusercontent.com/orisgh/nox.user.js/master/dist/w.user.js
+
+// @grant              none
+
+// @connect            none
+
+// @require            none
+
+// @match              https://www.bilibili.com/video/online.html
+
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	let blockList = `
+
+oooooooooooooooo1111
+
+oooooooooooooooo公开课
+一数
+宋浩老师官方
+一化儿
+刘文超Vin
+CocoPolaris
+黑马程序员
+Python_子木
+威猛公考
+江协科技
+
+oooooooooooooooo游戏
+是大腿
+STN工作室
+野水游言
+原神
+你的影月月
+莴苣某人
+明日方舟
+萧然Q
+魔法Zc目录
+苏烟Suyn
+孜然酱酱酱
+布锅锅
+崩坏星穹铁道
+分析师蓝宝石
+老实憨厚的笑笑
+烂活电竞
+CSGO-QUQU
+游民星空官方
+怕上火暴王老菊
+尕丶天堂
+
+oooooooooooooooo生活区
+雨哥到处跑
+某幻君
+中国BOY超级大猩猩
+小潮院长
+高斯Goh
+TF家族
+逗比的雀巢
+
+oooooooooooooooo汽车
+懂车帝原创
+是语汐
+
+oooooooooooooooo娱乐
+泥萌哪噶
+
+oooooooooooooooo阴谋论
+啊粥粥啊粥
+
+oooooooooooooooo哔哩官方
+哔哩哔哩国创
+哔哩哔哩英雄联盟赛事
+哔哩哔哩番剧
+哔哩哔哩电影
+
+oooooooooooooooo9999
+
+`.trim().split('\n');
+
+	function aaaaaaaa() {
+		let eboxes = document.querySelectorAll('.ebox');
+		eboxes.forEach(ebox => {
+			let author = ebox.querySelector('.author');
+			if (blockList.includes(author.textContent)) {
+				ebox.style.display = 'none';
+			}
+		});
+	}
+
+	setTimeout(aaaaaaaa, 5000);
+
+	document.addEventListener("visibilitychange", function () {
+		if (document.visibilityState === "visible") {
+			setTimeout(aaaaaaaa, 1000);
+		}
+	});
+
+	let ctrlDown = false;
+	let otherKeyDown = false;
+
+	document.addEventListener('keydown', function(event) {
+		if (event.key === 'Control') {
+			ctrlDown = true;
+		} else {
+			otherKeyDown = true;
+		}
+	});
+
+	document.addEventListener('keyup', function(event) {
+		if (event.key === 'Control') {
+			if (ctrlDown && !otherKeyDown) {
+				aaaaaaaa();
+			}
+			ctrlDown = false;
+			otherKeyDown = false;
+		} else {
+			otherKeyDown = false;
+		}
+	});
+
+})();
