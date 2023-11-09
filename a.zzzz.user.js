c297cbb2658586b04b5572d0859ebfd436ddfb90
orisgh <44023550+orisgh@users.noreply.github.com>
2023-11-09 08:54:52 +0800

    2023.1109

---
+++ a.zzzz.user.js
@@ -0,0 +1,72 @@
+// ==UserScript==
+
+// @name               a.zzzz
+// @downloadURL        https://github.com/orisgh/nox.user.js/raw/main/a.zzzz.user.js
+// @updateURL          https://github.com/orisgh/nox.user.js/raw/main/a.zzzz.user.js
+// @run-at             document-idle
+// @version            2023.11
+
+// @description        w
+// @author             nox
+// @license            MIT
+// @namespace          https://github.com/orisgh
+// @copyright          2023, nox (https://github.com/orisgh)
+// @homepageURL        https://github.com/orisgh/nox.user.js
+
+// @noframes
+// @grant              none
+// @connect            none
+// @require            none
+
+// @match              <$URL$>
+
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function aaaaaaaa() {
+
+        // code
+
+    }
+
+    aaaaaaaa();
+
+    setTimeout(aaaaaaaa, 4000);
+    setTimeout(aaaaaaaa, 8000);
+
+    document.addEventListener("visibilitychange", function () {
+        if (document.visibilityState === "visible") {
+            setTimeout(aaaaaaaa, 1000);
+        }
+    });
+
+    document.addEventListener('keydown', function(event) {
+        if (event.altKey) {
+            aaaaaaaa();
+        }
+    });
+
+    let ctrlDown = false;
+    let otherKeyDown = false;
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Control') {
+            ctrlDown = true;
+        } else {
+            otherKeyDown = true;
+        }
+    });
+    document.addEventListener('keyup', function (event) {
+        if (event.key === 'Control') {
+            if (ctrlDown && !otherKeyDown) {
+                aaaaaaaa();
+            }
+            ctrlDown = false;
+            otherKeyDown = false;
+        } else {
+            otherKeyDown = false;
+        }
+    });
+
+})();
